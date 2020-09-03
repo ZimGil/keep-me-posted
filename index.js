63dd0bf1e4c54bb6c468604cea9ac0c0a4c78421
ZimGil <gil.tichon@gmail.com>
2020-09-03 19:51:29 +0300

    Allow array of chat IDs

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ if (os.arch().includes('arm')) {
 /**
  * Execute your callback on a Webpage, sends a Telegram Message using your Bot.
  *
- * @param {String}   URL           Webpage URL on which the callback will be executed.
- * @param {Object}   settings       Settings for telegram bot usage or return value.
- * @param {string}   settings.telegramBotToken Telegram Bot API Token (from BotFather).
- * @param {string | number} settings.cahtId     Your telegram chat ID.
- * @param {function} callback Function to execute on the Webpage.
- * @param {...any} [args] Parameters for the callback function.
+ * @param {String}                                URL                         Webpage URL on which the callback will be executed.
+ * @param {Object}                                settings                    Settings for telegram bot usage or return value.
+ * @param {string}                                settings.telegramBotToken   Telegram Bot API Token (from BotFather).
+ * @param {string | number | string[] | number[]} settings.cahtId             Your telegram chat ID or an Array of IDs.
+ * @param {function}                              callback                    Function to execute on the Webpage.
+ * @param {...any}                                [args]                      Parameters for the callback function.
  *
  * @return {Promise<string>} Promise object that resolves with the Message return by the Callback function.
  */
@@ -33,7 +33,8 @@ module.exports = async function keepMePosted(URL, settings, callback, ...args) {
   if (settings.type === 'telegram') {
     const client = Telegram.TelegramClient.connect(settings.telegramBotToken);
     message = message.replace(/[_\*\[\]\(\)~`>#+-=|{}\.!]/g, (s) => `\\${s}`);
-    await client.sendMessage(settings.chatId, message, { parse_mode: 'MarkdownV2' });
+    const ids = Array.isArray(settings.cahtId) ? settings.cahtId : [settings.cahtId];
+    ids.forEach(async (id) => await client.sendMessage(id, message, { parse_mode: 'MarkdownV2' }));
     return true;
   }
 
